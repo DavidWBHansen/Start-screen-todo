3fd5fe450e481560f4e4ef80746c14d0bd1a577c
DavidWBHansen <David214@live.dk>
2021-05-09 22:51:16 +0200

    Fixed

--- js/todo.js
+++ js/todo.js
@@ -32,7 +32,7 @@ function myEventHandler(e){
 function show() {
     var todos = get_todos();
 
-    var html = '<ul>';
+    var html = '<ul id="id01">';
     for(var i=0; i<todos.length; i++) {
         html += '<li class="remove" id="' + i  + '">' + todos[i] + '</li>';
     };
@@ -57,13 +57,47 @@ function remove() {
     return false;
 }
 
+function sortList() {
+  var list, i, switching, b, shouldSwitch;
+  list = document.getElementById("id01");
+  switching = true;
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // start by saying: no switching is done:
+    switching = false;
+    b = list.getElementsByTagName("LI");
+    // Loop through all list-items:
+    for (i = 0; i < (b.length - 1); i++) {
+      // start by saying there should be no switching:
+      shouldSwitch = false;
+      /* check if the next item should
+      switch place with the current item: */
+      if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+        /* if next item is alphabetically
+        lower than current item, mark as a switch
+        and break the loop: */
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark the switch as done: */
+      b[i].parentNode.insertBefore(b[i + 1], b[i]);
+      switching = true;
+    }
+  }
+}
+
+
 show();
 $(document).ready(function() {
  $('#task').keypress(function(event) {
 
 if (event.which == 13){
-  add();
- 
+  add(); 
+
     }
     });
 });
